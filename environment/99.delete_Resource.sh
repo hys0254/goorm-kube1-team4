8672e3f934585da8e0d22d8475a78a4d1e6a3458
ShimKi <shm8934@gmail.com>
2021-11-13 02:37:33 +0900

    리소스 삭제 스크립트 추가 / 리소스로 생성된 스택 삭제 영역도 추가

--- environment/99.delete_Resource.sh
+++ environment/99.delete_Resource.sh
@@ -3,38 +3,58 @@
 # 클러스터 스택 삭제 직전, 리소스 삭제용 
 ## 스택 이름 얻기
 ALBStackName=`aws cloudformation list-stacks --stack-status-filter "CREATE_COMPLETE" --query='StackSummaries[].StackName' --output json | jq -r '.[]' | grep 'aws-load-balancer-controller'`
-CSIStackName=`aws cloudformation list-stacks --stack-status-filter "CREATE_COMPLETE" --query='StackSummaries[].StackName' --output json | jq -r '.[]' | grep 'ebs-csi-controller-sa'`
+EBSStackName=`aws cloudformation list-stacks --stack-status-filter "CREATE_COMPLETE" --query='StackSummaries[].StackName' --output json | jq -r '.[]' | grep 'ebs-csi-controller-sa'`
+EFSStackName=`aws cloudformation list-stacks --stack-status-filter "CREATE_COMPLETE" --query='StackSummaries[].StackName' --output json | jq -r '.[]' | grep 'efs-csi-controller-sa'`
+
+echo 'ALBStackName : '${ALBStackName}
+echo 'EBSStackName : '${EBSStackName}
+echo 'EFSStackName : '${EFSStackName}
+
+## ARN 정보 얻기
+ALBARN=`aws cloudformation describe-stacks --stack-name ${ALBStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]'`
+EBSARN=`aws cloudformation describe-stacks --stack-name ${EBSStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]'`
+EFSARN=`aws cloudformation describe-stacks --stack-name ${EFSStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]'`
+echo 'ALBARN : '${ALBARN}
+echo 'EBSARN : '${EBSARN}
+echo 'EFSARN : '${EFSARN}
 
 ## RoleName 얻는 명령문 개별적으로 실행해 볼 땐, 감싸고 있는 ``를 제거하고 OutputKey=='Role1' 으로 수정하고 해야됩니다!
 ALBRoleName=`aws cloudformation describe-stacks --stack-name ${ALBStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]' | cut -f 2 -d "/"`
-CSIRoleName=`aws cloudformation describe-stacks --stack-name ${CSIStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]' | cut -f 2 -d "/"`
-
+EBSRoleName=`aws cloudformation describe-stacks --stack-name ${EBSStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]' | cut -f 2 -d "/"`
+EFSRoleName=`aws cloudformation describe-stacks --stack-name ${EFSStackName} --query='Stacks[].Outputs[?OutputKey==\`Role1\`].OutputValue' --output json | jq -r '.[0] | .[0]' | cut -f 2 -d "/"`
+echo 'ALBRoleName : '${ALBRoleName}
+echo 'EBSRoleName : '${EBSRoleName}
+echo 'EFSRoleName : '${EFSRoleName}
 
 echo '>>>> Delete Resources Start '
 
 # 단계 1 - csi 드라이버 관련 리소스 삭제
-echo '> Step 1 : Delete CSI-Driver Resource Start '
+echo '> Step 1 : Delete EBS-Driver Resource Start '
 ## kubernetes 리소스 삭제
-echo '>> delete csi-controller '
-kubectl delete -k CSI/aws-ebs-csi-driver/deploy/kubernetes/base/
-## CSI Policy attach Role 삭제
-echo '>> delete CSI Role '
-aws iam delete-role --role-name ${CSIRoleName}
-echo ' Delete CSI-Driver Resource Finished '
+echo '>> delete storageclass.yaml'
+kubectl delete -f CSI/EFS/storageclass.yaml
 
-# 단계 2 - alb 드라이버 관련 리소스 삭제
+echo '>> delete EFS-csi-controller '
+kubectl delete -f CSI/EFS/driver.yaml
+
+echo '>> delete EBS-csi-controller '
+kubectl delete -k CSI/ESB/aws-ebs-csi-driver/deploy/kubernetes/base/
+
+# 단계 3 - alb 드라이버 관련 리소스 삭제
 echo '> Step 2 : Delete ALB Resource Start '
 ## kubernetes alb controller deployment 삭제
 kubectl delete deployment -n kube-system aws-load-balancer-controller
-## ALB Policy attach Role 삭제
-echo '>> delete ALB Role '
-aws iam delete-role --role-name ${ALBRoleName}
-echo ' Delete ALB Resource Finished '
-
 echo '>>>> Delete Resources Finished '
 
-# 단계 3 - CSI, ALB 관련 폴더 삭제
-echo '> Step 3 : Delete Download Resources associated with CSI, ALB <'
+# 단계 4 - 스택 삭제
+echo '>> Delete Resources stack Start'
+aws cloudformation delete-stack --stack-name ${EFSStackName} --role-arn ${EFSARN}
+aws cloudformation delete-stack --stack-name ${EBSStackName} --role-arn ${EBSARN}
+aws cloudformation delete-stack --stack-name ${ALBStackName} --role-arn ${ALBARN}
+
+# 단계 5 - EBS, ALB 관련 폴더 삭제
+echo '> Step 3 : Delete Download Resources associated with EBS, ALB <'
 rm -rf CSI
 rm -rf ALB
 
+echo '>> Delete Resource, Stack Finished -> Delete Cluster Able'
